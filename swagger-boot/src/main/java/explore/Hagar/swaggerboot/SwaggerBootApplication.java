0fa52429f57f9ecb85b6e690ea10249dc7372d2e
Hagar Usama <eng-hagar.usama1621@alexu.edu.eg>
2024-03-29 17:52:50 +0200

    init springboot

---
+++ swagger-boot/src/main/java/explore/Hagar/swaggerboot/SwaggerBootApplication.java
@@ -0,0 +1,13 @@
+package explore.Hagar.swaggerboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggerBootApplication.class, args);
+	}
+
+}
---
+++ swagger-boot/src/test/java/explore/Hagar/swaggerboot/SwaggerBootApplicationTests.java
@@ -0,0 +1,13 @@
+package explore.Hagar.swaggerboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwaggerBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
